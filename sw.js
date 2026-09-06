22a9866a277216e6498ecc59df887fd06a6ec4ee
williamgoodhew <32809621+williamgoodhew@users.noreply.github.com>
2026-09-06 12:09:00 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const C = 'stretch-v1';
+const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(C).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(k => Promise.all(k.filter(n => n !== C).map(n => caches.delete(n)))));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
