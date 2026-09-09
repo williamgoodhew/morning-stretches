490e0f03e3b5ba6da4eb8681a24f120b0c1e8c1b
williamgoodhew <32809621+williamgoodhew@users.noreply.github.com>
2026-09-09 14:56:12 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = 'stretch-v4';
+const C = 'stretch-v5';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(C).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
